dde506664d60919be487b5b13619ecf16c375d21
ramtin.zand <ramtinzand6@gmail.com>
2023-10-14 17:44:49 +0330

    first

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<RegisterForm />} />
-      <Route path="/login/:userId" element={<ShowData />} />
+      <Route path="/login" element={<ShowData />} />
     </Routes>
   );
 };
--- frontend/src/components/data/ShowData.jsx
+++ frontend/src/components/data/ShowData.jsx
@@ -1,40 +1,10 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import {getInfoService } from "../../services/userService";
+
 
 const ShowData = () => {
-    const { userId } = useParams();
-
-  const [userInfo, setuserInfo] = useState([]);
-  const [dinnerInfo, setdinnerInfo] = useState([]);
-
-  useEffect(() => {
-    const getinfo = async () => {
-      const { data } = await getInfoService(userId);
-
-      setuserInfo(data.user);
-      setdinnerInfo(data.dinner);
-    };
-    getinfo();
-  }, [userId]);
+    
   return (
     <div>
-      {userInfo.map((user) => (
-        <div className="" key={user._id}>
-          <p>
-            اطلاعات فردی : {user.name} {user.lastname}
-          </p>
-        </div>
-      ))}
-
-      {dinnerInfo.map((dinner) => (
-        <div className="" key={dinner._id}>
-          <p className="">
-            اطلاعات شام : {dinner.dinnerStatus} {dinner.drinksStatus}
-          </p>
-          <p>مجموع قیمت پرداختی : {dinner.price}</p>
-        </div>
-      ))}
+      hi
     </div>
   );
 };
