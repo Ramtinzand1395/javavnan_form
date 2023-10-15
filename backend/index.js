b0a2f7b74d265ff5e0261a1600125bdd53bbd2cb
ramtin.zand <ramtinzand6@gmail.com>
2023-10-15 19:11:14 +0330

    first

--- backend/index.js
+++ backend/index.js
@@ -15,11 +15,15 @@ connectDB();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.use("/api", require("./routes/userRoutes"));
 app.use("/api", require("./routes/adminRoutes"));
 
