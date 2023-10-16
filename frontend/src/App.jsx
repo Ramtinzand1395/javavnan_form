abe6567d6bb253f84126f05cb9119e45dbd613fb
ramtin.zand <ramtinzand6@gmail.com>
2023-10-16 11:10:53 +0330

    first

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,15 @@
-import {
-  ShowData,
-} from "./components";
-import { Route, Routes } from "react-router-dom";
+import { ShowData } from "./components";
+import { HashRouter as Router, Route } from "react-router-dom";
 import RegisterForm from "./components/RegisterForm";
 import DeleteMe from "./components/data/DeleteMe";
 
 const App = () => {
-
   return (
-    <Routes>
+    <Router>
       <Route path="/" element={<RegisterForm />} />
       <Route path="/login/:userId" element={<ShowData />} />
       <Route path="/dash" element={<DeleteMe />} />
-
-    </Routes>
+    </Router>
   );
 };
 
