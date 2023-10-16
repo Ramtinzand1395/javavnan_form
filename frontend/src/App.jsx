65d6f3a07e6c62c03ddf562c8319fd5c61fd3a7e
ramtin.zand <ramtinzand6@gmail.com>
2023-10-16 11:17:41 +0330

    first

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { ShowData } from "./components";
-import {HashRouter as Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import RegisterForm from "./components/RegisterForm";
 import DeleteMe from "./components/data/DeleteMe";
 
