dff29dd983185afae61aa9272408eab6f3eb5501
ramtin.zand <ramtinzand6@gmail.com>
2023-10-16 20:08:54 +0330

    first

--- frontend/src/components/steps/Acount.jsx
+++ frontend/src/components/steps/Acount.jsx
@@ -1,22 +1,22 @@
-import { useContext, } from "react";
+import { useContext, useState } from "react";
 import { Field, ErrorMessage, Formik, Form } from "formik";
-import {
-  registerUserService,
-} from "../../services/userService";
+import { registerUserService } from "../../services/userService";
 import { toast } from "react-toastify";
 import { StepperContext } from "../context/StepperContext";
 
 const Acount = () => {
-  const {setUserId , user, setuser , UserId } = useContext(StepperContext);
-
+  const { setUserId, user, setuser, UserId } = useContext(StepperContext);
+  const [loading, setloading] = useState(false);
   const registerUser = async (values) => {
     try {
-      const { data, status } = await registerUserService(values , UserId);
+      const { data, status } = await registerUserService(values, UserId);
       if (status === 201) {
+        setloading(true);
         toast.success(data.message);
         setUserId(data.userId);
         setuser(data.user);
       }
+      setloading(false);
     } catch (err) {
       console.log(err);
     }
@@ -36,14 +36,14 @@ const Acount = () => {
         >
           <Form>
             <label
-              className={`block md:text-base font-medium text-xs mb-2 text-black font-vazir text-start`}
+              className={`block md:text-base font-bold text-xs mb-2 text-white font-tanha text-start`}
             >
               نام
             </label>
             <Field
               name="name"
               type="text"
-              className="p-2 mb-5 px-2 appearance-none outline-none w-full text-white bg-gray-400 focus:bg-gray-600 border border-black text-base rounded-lg"
+              className="p-2 mb-5 px-2 appearance-none outline-none w-full text-white bg-white-400 focus:bg-gray-600 border border-black text-base rounded-lg"
             />
 
             <ErrorMessage
@@ -51,14 +51,14 @@ const Acount = () => {
               render={(msg) => <div className="text-red-500 m-4">{msg}</div>}
             />
             <label
-              className={`block md:text-base font-medium text-xs mb-2 text-black font-vazir text-start`}
+              className={`block md:text-base font-bold text-xs mb-2 text-white font-tanha text-start`}
             >
               نام خانوادگی
             </label>
             <Field
               name="lastname"
               type="text"
-              className="p-2 mb-5 px-2 appearance-none outline-none w-full text-white bg-gray-400 focus:bg-gray-600 border border-black text-base rounded-lg"
+              className="p-2 mb-5 px-2 appearance-none outline-none w-full text-white bg-white-400 focus:bg-gray-600 border border-black text-base rounded-lg"
             />
 
             <ErrorMessage
@@ -68,9 +68,9 @@ const Acount = () => {
             <div className="mx-2 flex items-start flex-col">
               <button
                 type="submit"
-                className="w-full py-2 text-white bg-blue-400 font-vazir mt-5 rounded-full hover:bg-blue-500 hover:border border-black transition-all delay-75"
+                className="w-full py-2 text-white bg-blue-400 font-vazir mt-5 rounded-full hover:bg-blue-500 border border-white transition-all delay-75"
               >
-                ثبت اطلاعات
+                {loading ? " در حال ثبت نام " : " ثبت اطلاعات"}
               </button>
             </div>
           </Form>
