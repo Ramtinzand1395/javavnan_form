29cd23bc6a841f05f88833be2e7f377b6fa64c7e
ramtin.zand <ramtinzand6@gmail.com>
2023-11-11 13:34:07 +0330

    zarin

--- frontend/src/components/steps/Finall.jsx
+++ frontend/src/components/steps/Finall.jsx
@@ -1,29 +1,37 @@
 import React, { useEffect, useState } from "react";
 import { paymentresponseService } from "../../services/userService";
+import { useNavigate } from "react-router-dom";
 
 const Finall = ({ Authority, Status }) => {
   const [PayStatus, setPayStatus] = useState("");
+  const navigate = useNavigate();
   useEffect(() => {
     const getdata = async () => {
       try {
-        const { data , status } = await paymentresponseService({
+        const { data, status } = await paymentresponseService({
           query: { Authority, Status },
         });
-        if(status === 200){
-          setPayStatus("ok")
+        if (status === 200) {
+          setPayStatus(data);
         }
       } catch (err) {
         console.log(err);
-        setPayStatus("Nok");
+        setPayStatus(err.response.data);
       }
     };
     getdata();
   }, [Authority, Status]);
 
+  const handleback = () => {
+    navigate("/");
+  };
   return (
     <div>
       <p className="text-white">
         {PayStatus}
+        <button className="px-1 py-4 bg-green-500" onClick={() => handleback()}>
+          بازگشت به صفحه اصلی
+        </button>
       </p>
     </div>
   );
