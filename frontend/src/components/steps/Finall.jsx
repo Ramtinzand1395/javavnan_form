15dc75b6aa642a026260c8f699d4e395aae9b27c
ramtin.zand <ramtinzand6@gmail.com>
2023-11-12 12:37:47 +0330

    price2

--- frontend/src/components/steps/Finall.jsx
+++ frontend/src/components/steps/Finall.jsx
@@ -49,6 +49,7 @@ const Finall = ({ Authority, Status }) => {
           }`}
         >
           {PayStatus}
+          1
         </p>
         {Isvalid ? (
           <div className="bg-white p-5 text-center font-vazir text-2xl">
