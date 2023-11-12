99a32aca66fbb285d3cb637bb7e0dcf2e5862254
ramtin.zand <ramtinzand6@gmail.com>
2023-11-12 07:44:41 +0330

    zarin

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,13 @@ app.use(
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://kulucheh.ir');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use("/api", require("./routes/adminRoutes"));
 
 const port = process.env.PORT || 5000;
